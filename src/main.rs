5f69b29c4ace7adbf6d34e43a864086f6df4e815
Kremilly <contact@kremilly.com>
2024-07-25 07:25:41 -0300

    chore: add anyhow dependency and capture webpage screenshot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use anyhow::Result;
+
+use headless_chrome::{
+    Browser, 
+    LaunchOptions,
+    protocol::cdp::Page::CaptureScreenshotFormatOption, 
+};
+
+fn main() -> Result<()> {
+    let mut options = LaunchOptions::default_builder()
+        .build()
+        .expect("Couldn't find appropriate Chrome binary.");
+
+    options.window_size = Some((1920, 1080));
+
+    let browser = Browser::new(options)?;
+    let tab = browser.new_tab()?;
+    
+    let png_data = tab
+        .navigate_to("https://www.wikipedia.org")?
+        .wait_until_navigated()?
+        .capture_screenshot(CaptureScreenshotFormatOption::Png, None, None, true)?;
+
+    fs::write("screenshot.png", png_data)?;
+
+    println!("Screenshots successfully created.");
+    Ok(())
 }
